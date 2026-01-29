3dce3eb814c28e5e9bc819997de487fd367ebd63
luka <lukaonik@gmail.com>
2026-01-29 13:15:20 +0400

    add style

--- playground/src/index.tsx
+++ playground/src/index.tsx
@@ -2,6 +2,8 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 
+import "./style.css";
+
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
